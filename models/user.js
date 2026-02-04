a012f778c1833f1aab7fccc454a862b1dc95051a
vnkt-prjct <durgaprasadbadri999@gmail.com>
2026-02-04 23:48:04 +0530

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user"
+  },
+
+  mfaEnabled: {
+    type: Boolean,
+    default: false
+  },
+
+  mfaSecret: {
+    type: String
+  },
+
+  resetToken: {
+    type: String
+  },
+
+  resetExpires: {
+    type: Date
+  }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", UserSchema);
